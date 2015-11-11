e5bfef2bd7277cb2faefecc2a3d670d05582b187
Mickael <mickael@rehabstudio.com>
2015-11-11 14:06:02 +0000

    using react-isomorphic-starterkit

--- src/client.js
+++ src/client.js
@@ -1,67 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Router} from "react-router";
+import Transmit from "react-transmit";
+import routes from "views/routes";
+import {createHistory} from "history";
+
 /**
-* THIS IS THE ENTRY POINT FOR THE CLIENT, JUST LIKE server.js IS THE ENTRY POINT FOR THE SERVER.
-*/
-import 'babel/polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import createHistory from 'history/lib/createBrowserHistory';
-import createStore from './redux/create';
-import ApiClient from './helpers/ApiClient';
-import io from 'socket.io-client';
-import {Provider} from 'react-redux';
-import {reduxReactRouter, ReduxRouter} from 'redux-router';
+ * Fire-up React Router.
+ */
+const reactRoot = window.document.getElementById("react-root");
+Transmit.render(Router, {routes, history: createHistory()}, reactRoot);
 
-import getRoutes from './routes';
-import makeRouteHooksSafe from './helpers/makeRouteHooksSafe';
-
-const client = new ApiClient();
-
-const dest = document.getElementById('content');
-const store = createStore(reduxReactRouter, makeRouteHooksSafe(getRoutes), createHistory, client, window.__data);
-
-function initSocket() {
-    const socket = io('', {path: '/api/ws', transports: ['polling']});
-    socket.on('news', (data) => {
-        console.log(data);
-        socket.emit('my other event', { my: 'data from client' });
-    });
-    socket.on('msg', (data) => {
-        console.log(data);
-    });
-
-    return socket;
+/**
+ * Detect whether the server-side render has been discarded due to an invalid checksum.
+ */
+if (process.env.NODE_ENV !== "production") {
+	if (!reactRoot.firstChild || !reactRoot.firstChild.attributes ||
+	    !reactRoot.firstChild.attributes["data-react-checksum"]) {
+		console.error("Server-side React render was discarded. Make sure that your initial render does not contain any client-side code.");
+	}
 }
 
-global.socket = initSocket();
-
-const component = (
-    <ReduxRouter routes={getRoutes(store)} />
-);
-
-ReactDOM.render(
-    <Provider store={store} key="provider">
-        {component}
-    </Provider>,
-    dest
-);
-
-if (process.env.NODE_ENV !== 'production') {
-    window.React = React; // enable debugger
-
-    if (!dest || !dest.firstChild || !dest.firstChild.attributes || !dest.firstChild.attributes['data-react-checksum']) {
-        console.error('Server-side React render was discarded. Make sure that your initial render does not contain any client-side code.');
-    }
-}
-
-if (__DEVTOOLS__) {
-    const DevTools = require('./containers/DevTools/DevTools');
-    ReactDOM.render(
-        <Provider store={store} key="provider">
-            <div>
-                {component}
-                <DevTools />
-            </div>
-        </Provider>,
-        dest
-    );
-}
--- src/server.js
+++ src/server.js
@@ -1,130 +1,69 @@
-import Express from 'express';
-import React from 'react';
-import ReactDOM from 'react-dom/server';
-import config from './config';
-import favicon from 'serve-favicon';
-import compression from 'compression';
-import httpProxy from 'http-proxy';
-import path from 'path';
-import createStore from './redux/create';
-import ApiClient from './helpers/ApiClient';
-import Html from './helpers/Html';
-import PrettyError from 'pretty-error';
-import http from 'http';
-import SocketIo from 'socket.io';
+import koa from "koa";
+import proxy from "koa-proxy";
+import serve from "koa-static";
 
-import {ReduxRouter} from 'redux-router';
-import createHistory from 'history/lib/createMemoryHistory';
-import {reduxReactRouter, match} from 'redux-router/server';
-import {Provider} from 'react-redux';
-import qs from 'query-string';
-import getRoutes from './routes';
-import getStatusFromRoutes from './helpers/getStatusFromRoutes';
+import React from "react";
+import ReactDOM from "react-dom/server";
+import {RoutingContext, match} from "react-router";
+import {createLocation} from "history";
+import Transmit from "react-transmit";
 
-const pretty = new PrettyError();
-const app = new Express();
-const server = new http.Server(app);
-const proxy = httpProxy.createProxyServer({
-    target: 'http://' + (process.env.HOST || 'localhost') + ':' + config.apiPort,
-    ws: true
+import routes from "views/routes";
+
+const app      = koa();
+const hostname = process.env.HOSTNAME || "localhost";
+const port     = process.env.PORT || 8000;
+
+app.use(serve("static", {defer: true}));
+
+app.use(proxy({
+	host: "https://api.github.com",
+	match: /^\/api\/github\//i,
+	map: (path) => path.replace(/^\/api\/github\//i, "/")
+}));
+
+app.use(function *(next) {
+	const location = createLocation(this.path);
+	const webserver = process.env.NODE_ENV === "production" ? "" : "//" + hostname + ":8080";
+
+	yield ((callback) => {
+		match({routes, location}, (error, redirectLocation, renderProps) => {
+			if (redirectLocation) {
+				this.redirect(redirectLocation.pathname + redirectLocation.search, "/");
+				return;
+			}
+
+			if (error || !renderProps) {
+				callback(error);
+				return;
+			}
+
+			Transmit.renderToString(RoutingContext, renderProps).then(({reactString, reactData}) => {
+				let template = (
+						`<!doctype html>
+					<html lang="en-us">
+						<head>
+							<meta charset="utf-8">
+							<title>react-isomorphic-starterkit</title>
+							<link rel="shortcut icon" href="/favicon.ico">
+						</head>
+						<body>
+							<div id="react-root">${reactString}</div>
+						</body>
+					</html>`
+				);
+
+				this.type = "text/html";
+				this.body = Transmit.injectIntoMarkup(template, reactData, [`${webserver}/dist/client.js`]);
+
+				callback(null);
+			});
+		});
+	});
 });
 
-app.use(compression());
-app.use(favicon(path.join(__dirname, '..', 'static', 'favicon.ico')));
 
-app.use(require('serve-static')(path.join(__dirname, '..', 'static')));
-
-// Proxy to API server
-app.use('/api', (req, res) => {
-    proxy.web(req, res);
+app.listen(port, () => {
+	console.info("==> ✅  Server is listening");
+	console.info("==> 🌎  Go to http://%s:%s", hostname, port);
 });
-
-// added the error handling to avoid https://github.com/nodejitsu/node-http-proxy/issues/527
-proxy.on('error', (error, req, res) => {
-    let json;
-    if (error.code !== 'ECONNRESET') {
-        console.error('proxy error', error);
-    }
-    if (!res.headersSent) {
-        res.writeHead(500, {'content-type': 'application/json'});
-    }
-
-    json = {error: 'proxy_error', reason: error.message};
-    res.end(JSON.stringify(json));
-});
-
-app.use((req, res) => {
-    if (__DEVELOPMENT__) {
-        // Do not cache webpack stats: the script file would change since
-        // hot module replacement is enabled in the development env
-        webpackIsomorphicTools.refresh();
-    }
-    const client = new ApiClient(req);
-
-    const store = createStore(reduxReactRouter, getRoutes, createHistory, client);
-
-    function hydrateOnClient() {
-        res.send('<!doctype html>\n' +
-        ReactDOM.renderToString(<Html assets={webpackIsomorphicTools.assets()} store={store}/>));
-    }
-
-    if (__DISABLE_SSR__) {
-        hydrateOnClient();
-        return;
-    }
-
-    store.dispatch(match(req.originalUrl, (error, redirectLocation, routerState) => {
-        if (redirectLocation) {
-            res.redirect(redirectLocation.pathname + redirectLocation.search);
-        } else if (error) {
-            console.error('ROUTER ERROR:', pretty.render(error));
-            res.status(500);
-            hydrateOnClient();
-        } else if (!routerState) {
-            res.status(500);
-            hydrateOnClient();
-        } else {
-            // Workaround redux-router query string issue:
-            // https://github.com/rackt/redux-router/issues/106
-            if (routerState.location.search && !routerState.location.query) {
-                routerState.location.query = qs.parse(routerState.location.search);
-            }
-
-            store.getState().router.then(() => {
-                const component = (
-                    <Provider store={store} key="provider">
-                        <ReduxRouter/>
-                    </Provider>
-                );
-
-                const status = getStatusFromRoutes(routerState.routes);
-                if (status) {
-                    res.status(status);
-                }
-                res.send('<!doctype html>\n' +
-                ReactDOM.renderToString(<Html assets={webpackIsomorphicTools.assets()} component={component} store={store}/>));
-            }).catch((err) => {
-                console.error('DATA FETCHING ERROR:', pretty.render(err));
-                res.status(500);
-                hydrateOnClient();
-            });
-        }
-    }));
-});
-
-if (config.port) {
-    if (config.isProduction) {
-        const io = new SocketIo(server);
-        io.path('/api/ws');
-    }
-
-    server.listen(config.port, (err) => {
-        if (err) {
-            console.error(err);
-        }
-        console.info('----\n==> ✅  %s is running, talking to API server on %s.', config.app.title, config.apiPort);
-        console.info('==> 💻  Open http://%s:%s in a browser to view the app.', (process.env.HOST || 'localhost'), config.port);
-    });
-} else {
-    console.error('==>     ERROR: No PORT environment variable has been specified');
-}
