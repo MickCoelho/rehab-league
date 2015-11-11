320410b22d1a92f1ad4a5b87eead789c2f1f228d
Mickael <mickael@rehabstudio.com>
2015-11-11 15:41:00 +0000

    Cleaning

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -57,51 +57,53 @@ function fetchData(getState, dispatch) {
             return (
                 <div className={styles.app}>
                     <DocumentMeta {...config.app}/>
+
+                    <div className={styles.appContent}>
+                        {this.props.children}
+                    </div>
                 </div>
             );
         }
     }
 
+/*
+<Navbar fixedTop toggleNavKey={0}>
+<NavBrand>
+<IndexLink to="/" activeStyle={{color: '#33e0ff'}}>
+<div className={styles.brand}/>
+<span>React Redux Example</span>
+</IndexLink>
+</NavBrand>
 
+<CollapsibleNav eventKey={0}>
+<Nav navbar>
 
-                    /*
-                    <Navbar fixedTop toggleNavKey={0}>
-                        <NavBrand>
-                            <IndexLink to="/" activeStyle={{color: '#33e0ff'}}>
-                                <div className={styles.brand}/>
-                                <span>React Redux Example</span>
-                            </IndexLink>
-                        </NavBrand>
+<LinkContainer to="/about">
+    <NavItem eventKey={4}>About Us</NavItem>
+</LinkContainer>
 
-                        <CollapsibleNav eventKey={0}>
-                            <Nav navbar>
+{!user &&
+    <LinkContainer to="/login">
+        <NavItem eventKey={5}>Login</NavItem>
+    </LinkContainer>}
+    {user &&
+        <LinkContainer to="/logout">
+            <NavItem eventKey={6} className="logout-link" onClick={::this.handleLogout}>
+                Logout
+            </NavItem>
+        </LinkContainer>}
+    </Nav>
+    {user &&
+        <p className={styles.loggedInMessage + ' navbar-text'}>Logged in as <strong>{user.name}</strong>.</p>}
+            <Nav navbar right>
+                <NavItem eventKey={1} target="_blank" title="View on Github" href="https://github.com/erikras/react-redux-universal-hot-example">
+                    <i className="fa fa-github"/>
+                </NavItem>
+            </Nav>
+        </CollapsibleNav>
+    </Navbar>
 
-                                <LinkContainer to="/about">
-                                    <NavItem eventKey={4}>About Us</NavItem>
-                                </LinkContainer>
-
-                                {!user &&
-                                    <LinkContainer to="/login">
-                                        <NavItem eventKey={5}>Login</NavItem>
-                                    </LinkContainer>}
-                                    {user &&
-                                        <LinkContainer to="/logout">
-                                            <NavItem eventKey={6} className="logout-link" onClick={::this.handleLogout}>
-                                                Logout
-                                            </NavItem>
-                                        </LinkContainer>}
-                                    </Nav>
-                                    {user &&
-                                        <p className={styles.loggedInMessage + ' navbar-text'}>Logged in as <strong>{user.name}</strong>.</p>}
-                                            <Nav navbar right>
-                                                <NavItem eventKey={1} target="_blank" title="View on Github" href="https://github.com/erikras/react-redux-universal-hot-example">
-                                                    <i className="fa fa-github"/>
-                                                </NavItem>
-                                            </Nav>
-                                        </CollapsibleNav>
-                                    </Navbar>
-
-                                    <div className={styles.appContent}>
-                                        {this.props.children}
-                                    </div>
+    <div className={styles.appContent}>
+        {this.props.children}
+    </div>
 */
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router';
-const RaisedButton = require('material-ui/lib/raised-button');
-
+// const RaisedButton = require('material-ui/lib/raised-button');
 
 export default class Home extends Component {
     render() {
